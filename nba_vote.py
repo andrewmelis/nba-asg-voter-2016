6cbd8ef0f19eb005a0547311546caef7debf7b12
andrew melis <andrewmelis@gmail.com>
2016-01-10 21:00:24 -0600

    add timestamp to tweet to avoid duplicate tweet error

--- nba_vote.py
+++ nba_vote.py
@@ -1,4 +1,5 @@
 import os, twitter
+from datetime import datetime
 
 def nba_votes():
     consumer_key = os.environ.get('CONSUMER_KEY', "not a real key")
@@ -18,7 +19,7 @@ def login(consumer_key, consumer_secret, access_token_key, access_token_secret):
     return api
 
 def vote_for_player(api, player_name):
-    update_text = "{} #NBAVOTE".format(player_name)
+    update_text = "{} #NBAVOTE at {}".format(player_name, str(datetime.now()))
     return api.PostUpdate(update_text)
 
 nba_votes()
